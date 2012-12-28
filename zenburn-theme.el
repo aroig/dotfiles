ae0a8e1770389eab11ac2761d618fb4efe06b089
Abdo Roig-Maranges <abdo.roig@gmail.com>
2012-12-28 14:26:26 +0100

    tweaked latex faces

--- zenburn-theme.el
+++ zenburn-theme.el
@@ -244,12 +244,12 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(anzu-mode-line ((t (:foreground ,zenburn-cyan :weight bold))))
 ;;;;; auctex
    `(font-latex-bold-face ((t (:inherit bold))))
-   `(font-latex-warning-face ((t (:foreground nil :inherit font-lock-warning-face))))
-   `(font-latex-sectioning-5-face ((t (:foreground ,zenburn-red :weight bold ))))
+   `(font-latex-warning-face ((t (:foreground ,zenburn-sat-red))))
+   `(font-latex-sectioning-5-face ((t (:foreground ,zenburn-sat-orange-1 :weight bold :inherit fixed-pitch))))
    `(font-latex-sedate-face ((t (:foreground ,zenburn-yellow))))
-   `(font-latex-italic-face ((t (:foreground ,zenburn-cyan :slant italic))))
-   `(font-latex-string-face ((t (:inherit ,font-lock-string-face))))
-   `(font-latex-math-face ((t (:foreground ,zenburn-orange))))
+   `(font-latex-math-face ((t (:foreground ,zenburn-wood))))
+   `(font-latex-string-face ((t (:foreground ,zenburn-salmon))))
+   `(font-latex-slide-title-face ((t (:foreground ,zenburn-blue :weight bold))))
 ;;;;; auto-complete
    `(ac-candidate-face ((t (:background ,zenburn-bg+3 :foreground ,zenburn-bg-2))))
    `(ac-selection-face ((t (:background ,zenburn-blue-4 :foreground ,zenburn-fg))))
