0e43a543509e51a5cde1f7983328850f2db7f46d
Abdo Roig-Maranges <abdo.roig@gmail.com>
2013-02-22 18:12:06 +0100

    moved cairo-compmgr launch to xinitrc

--- rc.lua.d/autoload.lua
+++ rc.lua.d/autoload.lua
@@ -6,20 +6,6 @@
 
 local hostname = hostname
 
-exec("killall cairo-compmgr")                     -- kill all cairo instances
-
--- no compositing on galois. saves battery!
-if hostname ~= "galois" then
-    exec("cairo-compmgr -n")                      -- compositing manager
-end
-
-exec("xset dpms 0 0 0")                           -- disable power saving
-
--- not needed anymore
--- exec("xsetroot -cursor_name left_ptr")         -- set cursor right
-
--- Host specific stuff
--- if hostname == "grothendieck" then
--- elseif hostname == "hodge" then
--- elseif hostname == "galois" then
--- end
+-- exec("killall cairo-compmgr")                     -- kill all cairo instances
+-- exec("xset dpms 0 0 0")                           -- disable power saving
+-- exec("xsetroot -cursor_name left_ptr")            -- set cursor right. not needed anymore
