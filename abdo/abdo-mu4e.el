cdc436fcaa986f3944a5931a0625315abf823eb6
Abdo Roig-Maranges <abdo.roig@gmail.com>
2013-05-28 12:40:36 +0200

    more mu4e headers view tweaks

--- abdo/abdo-mu4e.el
+++ abdo/abdo-mu4e.el
@@ -130,11 +130,9 @@
   (setq mu4e-headers-time-format "%H:%M")
 
   ;; threading and duplicates
-  (setq mu4e-headers-show-threads t)
   (setq mu4e-headers-results-limit 500)
-
-  (setq mu4e-headers-include-related t)
-  (setq mu4e-headers-skip-duplicates t)
+  (setq mu4e-headers-show-threads t)
+  ; (setq mu4e-headers-skip-duplicates t)
 
   ;; fields in list view
   (setq mu4e-headers-fields
