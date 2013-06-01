0815337250a2cf515e4175311800f77313cded1d
Abdo Roig-Maranges <abdo.roig@gmail.com>
2013-06-01 19:56:49 +0200

    stop and reload awesome service

--- scripts/awesome.sh
+++ scripts/awesome.sh
@@ -2,4 +2,19 @@
 
 source /home/abdo/.environment
 source /home/abdo/.aliases
-/usr/bin/awesome &
+
+case "$1" in 
+    start)
+        /usr/bin/awesome &
+    ;;
+
+    stop)
+        echo "awesome.quit()" | /usr/bin/awesome-client
+    ;;
+
+    restart)
+        echo "awesome.restart()" | /usr/bin/awesome-client
+    ;;
+esac
+
+
