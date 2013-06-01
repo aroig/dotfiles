f5833f638e41b2af3449b4b4e6ebbccaceb551cb
Abdo Roig-Maranges <abdo.roig@gmail.com>
2013-06-01 21:11:06 +0200

    lock service

---
+++ scripts/mouse.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# get mouse ID
+mouseid=`xinput --list | perl -ne 'm/(Mouse|Touchpad)\s*id=([0-9]*)/ && print "$2"'`
+
+case "$1" in
+    lock)
+        xinput --set-prop "$mouseid" "Device Enabled" "0" 
+        ;;
+
+    unlock)
+        xinput --set-prop "$mouseid" "Device Enabled" "1" 
+        ;;
+esac
