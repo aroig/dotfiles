8ea01453ced5662b418fe6f6d2d42c8e41af317f
Abdo Roig-Maranges <abdo.roig@gmail.com>
2013-07-30 15:11:09 +0200

    ispell-dictionary-alist is fine on its own now

--- abdo/abdo-languages.el
+++ abdo/abdo-languages.el
@@ -18,16 +18,19 @@
   (setq ispell-program-name "aspell")
   (setq ispell-list-command "list")
 
+
   ;; Dictionary list in my way
-  (setq ispell-dictionary-alist
-        '(("german" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "de") nil utf-8)
-          ("spanish" "[[:alpha:]]" "[^[:alpha:]]" "" t ("-d" "es") nil utf-8)
-          ("english" "[[:alpha:]]" "[^[:alpha:]]" "'" t ("-d" "en") nil utf-8)
-          ("british" "[[:alpha:]]" "[^[:alpha:]]" "'" t ("-d" "en_GB") nil utf-8)
-          ("american" "[[:alpha:]]" "[^[:alpha:]]" "'" t ("-d" "en_US") nil utf-8)
-          ("catalan" "[[:alpha:]]" "[^[:alpha:]]" "['·-]" t ("-d" "ca") nil utf-8)
-          ("french" "[[:alpha:]]" "[^[:alpha:]]" "['·-]" t ("-d" "fr") nil utf-8)
-          ))
+  ;; NOTE: it seems ispell-dictionary-alist is fine now
+;  (setq ispell-dictionary-alist
+;        '((nil "[[:alpha:]]" "[^[:alpha:]]" "'" t ("-d" "en") nil utf-8)
+;          ("german" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "de") nil utf-8)
+;          ("spanish" "[[:alpha:]]" "[^[:alpha:]]" "" t ("-d" "es") nil utf-8)
+;          ("english" "[[:alpha:]]" "[^[:alpha:]]" "'" t ("-d" "en") nil utf-8)
+;          ("british" "[[:alpha:]]" "[^[:alpha:]]" "'" t ("-d" "en_GB") nil utf-8)
+;          ("american" "[[:alpha:]]" "[^[:alpha:]]" "'" t ("-d" "en_US") nil utf-8)
+;          ("catalan" "[[:alpha:]]" "[^[:alpha:]]" "['·-]" t ("-d" "ca") nil utf-8)
+;          ("french" "[[:alpha:]]" "[^[:alpha:]]" "['·-]" t ("-d" "fr") nil utf-8)
+;          ))
 
   ;; Don't ask to save personal dict
   (setq ispell-silently-savep t)
