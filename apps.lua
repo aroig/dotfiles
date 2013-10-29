46e73a164490f0cecd686d8cf590b80fb1df2518
Abdo Roig-Maranges <abdo.roig@gmail.com>
2013-10-29 23:02:25 +0100

    move exit.sh to bin subdir

--- apps.lua
+++ apps.lua
@@ -13,6 +13,7 @@ apps.terminal            = os.getenv("TERMCMD")     or "xterm"
 apps.editor              = os.getenv("EMACS")       or "emacs"
 apps.browser             = os.getenv("BROWSER")     or "dwb"
 apps.filemanager         = os.getenv("FILEMANAGER") or "thunar"
+apps.shell               = os.getenv("SHELL")       or "bash"
 
 -- Other apps
 apps.docbrowser          = apps.browser .. " -n"
@@ -32,7 +33,9 @@ apps.notes               = "emacs -notes"
 apps.chat                = "emacs -chat"
 
 -- System stuff
-apps.exit_cmd            = "bash ~/.config/awesome/exit.sh"
+local cfgdir = awful.util.getdir("config")
+
+apps.exit_cmd            = string.format("%s/bin/exit.sh", cfgdir)
 apps.lock_cmd            = "systemctl --user start lock.target"
 apps.suspend_cmd         = "sudo systemctl suspend"
 apps.poweroff_cmd        = "sudo systemctl poweroff"
