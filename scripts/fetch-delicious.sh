6b56ae487acee76db165c7806798b58404582b8c
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-01-11 23:00:58 +0100

    commit changes when fetching delicious bookmarks

--- scripts/fetch-delicious.sh
+++ scripts/fetch-delicious.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # url and paths
-url=https://api.del.icio.us/v1/posts/all
-dest=$HOME/var/delicious/$(date +%Y-%m-%d)-delicious.xml
-link=$HOME/var/delicious/delicious.xml
+url="https://api.del.icio.us/v1/posts/all"
+
+tgtdir="$HOME/var/delicious"
+dest="$tgtdir/delicious.xml"
 
 # user and password in netrc
-curl -S -s -L -n "$url" > "$dest"
-ln -sf $dest $link
+curl -S -s -L -n "$url" > "$dest" || exit 1
+
+git --git-dir="$tgtdir/.git" --work-tree="$tgtdir" add -A . && \
+git --git-dir="$tgtdir/.git" --work-tree="$tgtdir" commit -m 'updated bookmarks'
+exit 0
