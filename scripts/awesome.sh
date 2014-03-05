6373a34757ecb8e7f4e1cea78621f9214e561bcb
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-03-05 02:07:46 +0100

    kill dropdown clients before killing awesome

--- scripts/awesome.sh
+++ scripts/awesome.sh
@@ -5,8 +5,15 @@ source /home/abdo/.zshenv
 source /home/abdo/.aliases
 
 case "$1" in 
-    start)   exec /usr/bin/awesome > /dev/null ;;
-    stop)    echo "awesome.quit()" | /usr/bin/awesome-client ;;
-    restart) echo "awesome.restart()" | /usr/bin/awesome-client ;;
+    start) 
+        exec /usr/bin/awesome > /dev/null
+        ;;
+    stop)
+        echo "ddclient.kill_all()" | /usr/bin/awesome-client
+        echo "awesome.quit()" | /usr/bin/awesome-client 
+        ;;
+    restart)
+        echo "awesome.restart()" | /usr/bin/awesome-client 
+        ;;
 esac
         
