a74edb92e4ae145b7f4db6ffb084f703fe018fb7
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-03-09 19:09:33 +0100

    add dwb-db.service to commit dwb cookies
    I'll want to make it a dependency for the dwb transient units  I use.

---
+++ scripts/dwb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -o errexit
+
+arg="$1"
+dwb_path=$HOME/priv/var/dwb/default
+
+case $1 in 
+    start)       
+        exec dwb
+        ;;
+    
+
+    stop)
+        ;;
+
+
+    commit)
+        (
+            cd "$dwb_path"
+
+            # if there are changes, commit
+            num=$(git status --porcelain | wc -l)
+            if [[ $num -ge 1 ]]; then                
+                # add files
+                git add -A .
+                
+                # recompute the number of changes and commit
+                num=$(git status --porcelain | wc -l)
+                git commit -m "auto-commit: files changed ($num files)" > /dev/null
+            fi
+        )
+        ;;
+
+esac
