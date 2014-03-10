6243dd3511d2cab6d4073ce646be75ba458322b1
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-03-10 11:04:24 +0100

    add new script to commit repos

---
+++ scripts/git-commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -o errexit
+
+# Commit a git repo at the given path
+repo="$1"
+
+cd "$repo"
+
+# stop if it is not a git repo
+[ ! -d ".git" ] && exit 0
+
+# check for changes
+num=$(git status --porcelain | wc -l)    
+if [[ $num -ge 1 ]]; then            
+    echo "adding files to '`basename $repo`'"    
+    [ -d ".git/annex" ] && git annex add .
+    git add -A .
+                
+    # recompute the number of changes and commit
+    num=$(git status --porcelain | wc -l)
+    echo "commiting files to '`basename $repo`'"            
+    git commit -q -m "auto-commit ($num files)"
+fi
+
+
