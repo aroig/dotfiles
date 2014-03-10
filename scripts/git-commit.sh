976abcde4b56ace209cf02333801f4b182403a09
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-03-10 11:44:09 +0100

    git-commit.sh: echo the full path of the repo

--- scripts/git-commit.sh
+++ scripts/git-commit.sh
@@ -12,13 +12,13 @@ cd "$repo"
 # check for changes
 num=$(git status --porcelain | wc -l)    
 if [[ $num -ge 1 ]]; then            
-    echo "adding files to '`basename $repo`'"    
+    echo "adding files to '$repo'"    
     [ -d ".git/annex" ] && git annex add .
     git add -A .
                 
     # recompute the number of changes and commit
     num=$(git status --porcelain | wc -l)
-    echo "commiting files to '`basename $repo`'"            
+    echo "commiting files to '$repo'"            
     git commit -q -m "auto-commit ($num files)"
 fi
 
