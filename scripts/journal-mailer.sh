07dea777e5e71d37c8c2c77b6f36250348e83868
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-03-22 20:15:53 +0100

    journal-mailer.sh: delay quitting if a mail is being sent

--- scripts/journal-mailer.sh
+++ scripts/journal-mailer.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+mailer_quit() {
+    [ "$_exit" == "signal" ]  & exit 0
+    [ "$_exit" == "inhibit" ] & _exit=immediate
+}
+
+# trap SIGINT and SIGTERM so we make sure that we quit with all mail delivered
+trap mailer_quit SIGINT SIGTERM
+
+# if _exit=signal we quit ince SIGTERM is received
+# if _exit=inhibit we wait to quit until it is appropriate
+# if _exit=immediate we quit as we see it.
+
+_exit=signal
+
 journalctl -f -n0 -q --priority=3 SYSLOG_IDENTIFIER=systemd | \
 while read line; do
     user=$(id -nu)
@@ -8,6 +22,12 @@ while read line; do
 
     # mail information and wait
     echo "mailing fail report to user $user"
+
+    _exit=inhibit
     echo "$message" | mail -s "$subject" $user
+
+    [ "$_exit" == 'immediate'] & exit 0
+    _exit=signal
+
     sleep 60
 done
