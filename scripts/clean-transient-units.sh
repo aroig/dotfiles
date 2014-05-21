e86ac4f55fb3b0e901a75226c1da711f55d5d079
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-05-21 19:23:02 +0200

    clean more general transient units

--- scripts/clean-transient-units.sh
+++ scripts/clean-transient-units.sh
@@ -6,7 +6,7 @@ SYSTEMD_USER_DIR="$HOME/.config/systemd/user"
 # clean transient scopes
 echo "Cleaning transient units in $SYSTEMD_USER_DIR"
 
-find "$SYSTEMD_USER_DIR" -regex '.*/run-\([0-9]+\)\.\(scope\|service\)\(\.d\)?$' | \
+find "$SYSTEMD_USER_DIR" -regex '.*/run-\(.+\)\.\(scope\|service\)\(\.d\)?$' | \
 while read tunit; do
     rm -Rf "$tunit"
 done
