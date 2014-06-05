9a20f3a45c7fe86e0a6e7f17498bafb7de080c8b
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-06-05 22:33:19 +0200

    journal-mailer.sh: tweak subject of mail notification

--- scripts/journal-mailer.sh
+++ scripts/journal-mailer.sh
@@ -5,7 +5,7 @@ jctl_args="-n10 -b -q --priority=3 SYSLOG_IDENTIFIER=systemd"
 
 user=$(id -nu)
 host=$(hostname -s)
-subject="Errors detected while monitoring journal on '$host'"
+subject="[$host] Errors detected while monitoring journal"
 message="Journal:\n$(journalctl $jctl_args)\n"
 
 echo "mailing fail report to user $user"
