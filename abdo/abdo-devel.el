69f98d0172b609e7b3a8bef1c78db522f3f17947
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-06-22 15:05:03 +0200

    fix compile command again

--- abdo/abdo-devel.el
+++ abdo/abdo-devel.el
@@ -75,28 +75,39 @@
   (setq compilation-scroll-output 'first-error)
 )
 
-(defun get-closest-pathname (file)
-  "Determine the pathanem of the first instance of FILE starting
-from the current directory towards the root. This may not do the
-correct ting in the presence of links. If it does not find FILE,
-then it shall return the name of FILE in the current directory,
-suitable for creation."
-  (let ((root (expand-file-name "/"))) ; the win32 builds should translate this correctly
-	(expand-file-name file
-					  (loop
-					   for d = default-directory then (expand-file-name ".." d)
-					   if (file-exists-p (expand-file-name file d))
-					   return d
-					   if (equal d root)
-					   return nil))))
+
+(defun find-file-upwards (filename &optional startdir)
+  "Move up directories until we find a certain filename. If we
+  manage to find it, return the containing directory. Else if we
+  get to the toplevel directory and still can't find it, return
+  nil. Start at startdir or . if startdir not given"
+
+  (let ((dirname (expand-file-name (if startdir startdir ".")))
+        (found nil) ; found is set as a flag to leave loop if we find it
+        (top nil))  ; top is set when we get to /
+
+    ; While we've neither been at the top last time nor have we found the file.
+    (while (not (or found top))
+      ; If we're at / set top flag.
+      (if (string= (expand-file-name dirname) "/")
+          (setq top t))
+
+      ; Check for the file and move up one directory if not found
+      (if (file-exists-p (expand-file-name filename dirname))
+          (setq found t)
+        (setq dirname (expand-file-name ".." dirname))))
+
+    ; return statement
+    (if found dirname nil)))
+
 
 (defun abdo-devel-compile ()
   (interactive)
   (cond
    ((file-exists-p (concat default-directory "Makefile"))
     (compile "make -k"))
-   ((file-exists-p (get-closest-pathname "Makefile"))
-    (compile (format "make -k -C \"%s\"" (file-name-directory (get-closest-pathname "Makefile")))))
+   ((file-exists-p (find-file-upwards "Makefile"))
+    (compile (format "make -k -C \"%s\"" (find-file-upwards "Makefile"))))
    (t (message "Can't find a suitable Makefile"))))
 
 
