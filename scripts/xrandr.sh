3e16aacc16e5e89e750bf2c94ddddcd3fdae3508
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-09-05 17:16:30 +0200

    adapt xrandr.sh to galois

--- scripts/xrandr.sh
+++ scripts/xrandr.sh
@@ -11,36 +11,31 @@ OUTS=$(xrandr -q | grep "connected" | sed 's/^\([A-Za-z0-9\-]*\).*/\1/')
 # Connected outputs
 COUTS=$(xrandr -q | grep " connected" | sed 's/^\([A-Za-z0-9\-]*\).*/\1/')
 
-LVDS=$(echo $OUTS  | sed 's/\s/\n/g' | grep LVDS)
-DVI=$(echo $OUTS  | sed 's/\s/\n/g' | grep DVI)
-VGA=$(echo $OUTS  | sed 's/\s/\n/g' | grep VGA)
-HDMI=$(echo $OUTS  | sed 's/\s/\n/g' | grep HDMI)
+# match the right output
+match_out() {
+    echo $OUTS | sed 's/\s/\n/g' | grep "$1"
+}
 
 
 case $host in
     grothendieck)
-	    primary=$VGA
-	    secondary=$DVI
-	    tertiary=$HDMI
+	    primary=$(match_out VGA)
+	    secondary=$(match_out DVI)
+	    tertiary=$(match_out HDMI)
         dpms_timeout="0 0 0"
 	    ;;
     
     hodge)
-	    primary=$DVI
+	    primary=$(match_out DVI)
 	    secondary=""
 	    tertiary=""
         dpms_timeout="0 0 0"        
 	    ;;
     
     galois)
-	    primary=$LVDS
-	    if [ "$arg" == "hdmi" ]; then
-	        secondary=$HDMI
-	        tertiary=$VGA
-	    else
-	        secondary=$VGA
-	        tertiary=$HDMI
-	    fi
+	    primary=$(match_out eDP1)
+        secondary=$(match_out HDMI1)
+        tertiary=$(match_out HDMI2)
         dpms_timeout="300 600 900"
 	    ;;
 esac
