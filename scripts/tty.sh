6fdfa2c9d94d6060cac3564db954a56d7746880c
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-10-30 10:41:09 +0100

    fix set_tty_colors

--- scripts/tty.sh
+++ scripts/tty.sh
@@ -6,48 +6,46 @@
 #------------------------------------------------------------------#
 
 
-set_tty_colors_xresources() {
+list_colors_xresources() {
     # awk script to extract colors from urxvt config
     color_config="$HOME/.Xresources"
     awk_script='
     /urxvt\.color/ { 
         idx = gensub(/urxvt\.color([0-9]+):/, "\\1", "g", $1); 
         color = gensub(/#([0-9a-fA-F]+)/, "\\1", "g", $2);
-        printf("\\e]P%X%s\n", idx, toupper(color));
+        printf("%X%s\n", idx, toupper(color));
     }'
-    cat "$color_config" | awk "$awk_script" | \
-    while read color; do
-        echo -n "$color"
-    done
+    cat "$color_config" | awk "$awk_script"   
 }
 
 
-set_tty_colors_termite() {
+list_colors_termite() {
     # awk script to extract colors from termite config
     color_config="$HOME/.config/termite/config"
     awk_script='
     /^color/ { 
         idx = gensub(/color([0-9]+)/, "\\1", "g", $0); 
         color = gensub(/.*#([0-9a-fA-F]+)/, "\\1", "g", $0);
-        printf("\\e]P%X%s\n", idx, toupper(color));
+        printf("%X%s\n", idx, toupper(color));
     }'
-    cat "$color_config" | awk "$awk_script" | \
-    while read color; do
-        echo -n "$color"
-    done
+    cat "$color_config" | awk "$awk_script"
 }
 
 
-# TODO: something is wrong with this on zsh. Oh well...
+# TODO: something is wrong with this in graphical terminals
 set_tty_colors() {
-    
-    if [[ -e $HOME/.config/termite/config ]]; then
-        set_tty_colors_termite >> blah
+   
+    (
+        if [[ -e $HOME/.config/termite/config ]]; then
+            list_colors_termite
             
-    elif [[ -e "$HOME/.Xresources" ]]; then
-        set_tty_colors_xresources
-    fi
+        elif [[ -e "$HOME/.Xresources" ]]; then
+            list_colors_xresources
+        fi
+    ) | while read color; do
+            echo -e -n "\e]P$color"
+        done
  
-    echo -e "\\e]P0000000"
+    echo -e -n "\e]P0000000"
 }
 
