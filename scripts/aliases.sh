a97512ef63edde69ab3c0d71dcc2cb9bfce41b36
Abdo Roig-Maranges <abdo.roig@gmail.com>
2015-01-21 20:34:20 +0100

    update aliases

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -58,6 +58,7 @@ alias abye='ictl bye.target'
 alias pac='sudo pacman'
 alias pat='pactree -c'
 alias cow='cower'
+amr() { ( cd "$AB2_ARCH_DIR/$1"; shift; mr "$@"; ) }
 
 alias feh='feh --magick-timeout 2 --scale-down --fontpath /usr/share/fonts/TTF/ --font DejaVuSans/10'
 alias ipy='ipython'
