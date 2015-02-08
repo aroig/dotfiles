ddba9143b7e847e1e923c79dbe22bffcd07810c1
Abdo Roig-Maranges <abdo.roig@gmail.com>
2015-02-08 19:57:05 +0100

    improve sdrun script

--- bin/sdrun.sh
+++ bin/sdrun.sh
@@ -1,32 +1,82 @@
 #!/bin/bash
 
+# Defaults
 slice=apps.slice
-name=transient
 
-SYSTEMD_CONF="$HOME/.config/systemd"
 
-while getopts n:s: opt; do
+if [ $# == 0 ]; then
+    echo "usage: sdrun [<cmd> | <service>]"
+    echo ""
+    echo "Starts a command in a transient service, or an existing service template"
+    echo "instantiated at the first available integer parameter. "
+
+    exit 0
+fi
+
+
+
+available_index() {
+    local basename="$1"
+    local -i index
+    local unit
+    for index in $(seq 0 100); do
+        unit="$basename$index.service" 
+        if systemctl --user is-active "$unit" > /dev/null; then
+            continue;
+        fi
+        echo "$index"
+        return 0
+    done
+    
+    echo "Can't find a transient unit name available"
+    exit 1
+}
+
+case "$1" in
+    *@.service)
+        basename="${1%\.*}"
+        index=`available_index "$basename"`
+        unit="$basename$index"
+        desc="$basename $index"
+        cmd="systemctl --user start $unit"
+        ;;
+    *.service)
+        basename="${1%\.*}"
+        unit="$basename"
+        desc="$basename"
+        cmd="systemctl --user start $unit"
+        ;;
+    *)
+        basename="run-"`basename "$1"`"-"
+        index=`available_index "$basename"`
+        unit="$basename$index"
+        desc="transient "`basename "$1"`" $index"
+        cmd="systemd-run --user --slice=$slice --unit=$unit --description=\"$desc\" $@"
+        ;;
+esac
+
+while getopts n:u:s: opt; do
     case "$opt" in
-        n) name=$OPTARG  ;;
         s) slice=$OPTARG ;;
+        n) desc=$OPTARG ;;
+        u) unit=$OPTARG ;;
     esac
     shift
 done
 
-declare -i index
-for index in $(seq 0 100); do
-    unit="run-$name-$index.service"    
-    if systemctl --user is-active "$unit" > /dev/null; then continue; fi
-    if [ -e "$SYSTEMD_CONF/user/$unit" ];  then continue; fi
+case "$1" in
+    *@.service)
+        cmd="systemctl --user start $unit"
+        ;;
+    *.service)
+        cmd="systemctl --user start $unit"
+        ;;
+    *)
+        cmd="systemd-run --user --slice=\"$slice\" --unit=\"$unit\" --description=\"$desc\" $@"
+        ;;
+esac
 
-    desc="$name $index"
-
-    exec systemd-run --user --slice="$slice" --unit="$unit" --description="$desc" $@
-    exit 0
-done
-
-echo "Can't find a transient unit name available"
-exit 1
+eval "$cmd"
 
 
 
