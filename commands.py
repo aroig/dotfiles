eb7e3e035003906240f0142ed6778eb94591ff0d
Abdo Roig-Maranges <abdo.roig@gmail.com>
2015-02-08 21:54:52 +0100

    remove debugging code

--- commands.py
+++ commands.py
@@ -14,7 +14,6 @@ class autojump(Command):
     def execute(self):
         dirlist_raw = subprocess.check_output(["autojump", "--complete", self.arg(1)],
                                               env={'AUTOJUMP_SOURCED': '1'})
-        self.fm.notify(dirlist_raw)
         dirlist = dirlist_raw.decode("utf-8", "ignore").split('\n')
         if len(dirlist) > 0:
             directory = dirlist[0].strip()
