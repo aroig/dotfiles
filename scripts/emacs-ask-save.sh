04b310183222346db333839ace213e357183e0e9
Abdo Roig-Maranges <abdo.roig@gmail.com>
2015-02-10 13:47:55 +0100

    simplify emacs services

--- scripts/emacs-ask-save.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# NOTE: If emacs.service is not active, always succeed since there is nothing to be done.
-#       It would be nice to write the dependency on emacs.service directly in systemd!
-
-if systemctl --user is-active -q "$1"; then
-    exec emacsclient -s "$2"  --eval '(abdo-save-buffers-new-frame)' | grep nil > /dev/null
-fi
-
