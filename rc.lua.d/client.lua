38a1e99afae67f1f7ab1e70072e7f6e5f8414898
Abdo Roig-Maranges <abdo.roig@gmail.com>
2015-02-17 16:09:19 +0100

    the focus border issue is fixed upstream

--- rc.lua.d/client.lua
+++ rc.lua.d/client.lua
@@ -141,8 +141,7 @@ rules.rules = {
     -- All clients will match this rule.
     { rule = { }, except_any = { class = {"Tint2", "Plank"} },
       properties = { border_width = 1,
-                     -- NOTE: This races against focus handler.
-                     -- border_color = beautiful.border_normal,
+                     border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
                      raise=true,
                      keys = clientkeys,
