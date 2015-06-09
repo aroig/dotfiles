05990071997d87198e70daf222a0b2daa08237a2
Abdo Roig-Maranges <abdo.roig@gmail.com>
2015-06-09 03:11:15 +0200

    update machine shell aliases

--- scripts/machine-aliases.sh
+++ scripts/machine-aliases.sh
@@ -19,28 +19,6 @@ spwn() {
     fi
 }
 
-mchns() {
-    local machine="$1"
-    local config="$HOME/.config/systemd/conf/machines.d/$machine.conf"
-
-    if [ ! -f "$config" ]; then
-        echo "Can't find config file for machine $machine."
-        return 1
-    fi
-
-    case "$machine" in
-        tablet*|mobile*) systemctl --user start machine-android@$machine.service ;;
-        *)               systemctl --user start machine-ephimeral@$machine.service ;;
-    esac
-
-    source "$config"
-
-    # TODO: ideally I should use socket units to fix this race!
-    sleep 5
-    telnet localhost "$PORT"  
-}
-
-
 mchn() {
     local machine="$1"
     local config="$HOME/.config/systemd/conf/machines.d/$machine.conf"
@@ -54,6 +32,35 @@ mchn() {
         tablet*|mobile*) systemctl --user start machine-android@$machine.service ;;
         *)               systemctl --user start machine-ephimeral@$machine.service ;;
     esac
+
+    # TODO: ideally I should use socket units to fix this race!
+    sleep 2 
+}
+
+mchnv() {
+    local machine="$1"
+    mchn "$machine"
+    systemctl --user start machine-vnc@$machine.service
+}
+
+mchns() {
+    local machine="$1"
+    local config="$HOME/.config/systemd/conf/machines.d/$machine.conf"
+
+    mchn "$machine"
+
+    source "$config"
+    telnet localhost "$PORT"  
+}
+
+mchnc() {
+    local machine="$1"
+    local config="$HOME/.config/systemd/conf/machines.d/$machine.conf"
+
+    mchn "$machine"
+    
+    source "$config"
+    telnet localhost "$CTLPORT"
 }
 
 mchnk() {
