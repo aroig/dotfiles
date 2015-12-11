494d6c5115c865955ad66a0d7377abd411805f66
Abdo Roig-Maranges <abdo.roig@gmail.com>
2015-12-11 14:07:04 +0100

    add fwunban alias

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -216,6 +216,10 @@ fwban() {
     sudo iptables -A sshguard -s "$1" -j DROP
 }
 
+fwunban() {
+    sudo iptables -D sshguard -s "$1" -j DROP
+}
+
 # network
 gateway() {
     host `ip route list 0/0 | awk '{print $3}'` | awk '{print $5}'
