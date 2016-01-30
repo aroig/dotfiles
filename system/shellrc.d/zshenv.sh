ba2d79d15e8d310cbd11475171bfce656e725200
Abdo Roig-Maranges <abdo.roig@gmail.com>
2016-01-30 20:29:35 +0100

    add zshenv.sh to profile.d

--- system/shellrc.d/zshenv.sh
+++ system/shellrc.d/zshenv.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
 
 # source zshenv when not in zsh.
-if [ ! "$ZSH_VERSION" ]; then
-    if [ -f "/etc/zsh/zshenv" ]; then
-        source "/etc/zsh/zshenv"
-    fi
-
-    if [ -f "$HOME/.zshenv" ]; then
-        source "$HOME/.zshenv"
-    fi
+if [ -f "/etc/zsh/zshenv" ]; then
+    source "/etc/zsh/zshenv"
 fi
+
+if [ -f "$HOME/.zshenv" ]; then
+    source "$HOME/.zshenv"
+fi
+
