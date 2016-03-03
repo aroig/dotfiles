dfbeed1209027193042c0eab23caa945d7f56858
Abdo Roig-Maranges <abdo.roig@gmail.com>
2016-03-04 00:16:54 +0100

    add ab2-visuals layer

---
+++ layers/ab2-visuals/config.el
@@ -0,0 +1,11 @@
+
+(setq
+ ;; highlighting
+ transient-mark-mode t
+ show-paren-mode t
+ markdown-indent-on-enter nil
+ ;; modeline
+ display-time-format "%H:%M"
+ )
+
+(blink-cursor-mode t)
---
+++ layers/ab2-visuals/packages.el
@@ -0,0 +1,10 @@
+(setq ab2-visuals-packages
+      '(
+        spaceline
+        ))
+
+(defun ab2-visuals/pre-init-spaceline ()
+  (setq
+   spaceline-buffer-size-p nil
+   spaceline-line-column t
+   ))
