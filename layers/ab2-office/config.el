46efcd6a084c076fd7ccfa001dff8559265ee9ee
Abdo Roig-Maranges <abdo.roig@gmail.com>
2016-03-06 20:08:59 +0100

    remove ab2-office. I've split it into org latex and mu4e

--- layers/ab2-office/config.el
+++
@@ -1,8 +0,0 @@
-
-(setq
- browse-url-generic-program "xdg-open"
- browse-url-browser-function 'browse-url-generic
- )
-
-(defvar mu4e-action-tags-completion-list '()
-  "List of tags to show for autocompletion")
