1b91466b202375169d2260c499d7445865d184ff
Abdo Roig-Maranges <abdo.roig@gmail.com>
2016-03-11 15:05:16 +0100

    add override for chosing mu4e account

--- layers/ab2-mu4e/funcs.el
+++ layers/ab2-mu4e/funcs.el
@@ -66,6 +66,39 @@
 ;; Functions
 ;; -----------------------------------------------
 
+;; Overrides mu4e/set-account from the spacemacs layer.
+(defun mu4e/set-account ()
+  "Set the account for composing a message.
+This function tries to guess the correct account from the email address from the to and cc fields, or the maildir. If no unambiguous guess can be done, prompts the user."
+  (let* ((emails (when mu4e-compose-parent-message
+                   (mapcar (lambda (x) (downcase (cdr x)))
+                           (append
+                            (mu4e-message-field mu4e-compose-parent-message :to)
+                            (mu4e-message-field mu4e-compose-parent-message :cc)
+                            (mu4e-message-field mu4e-compose-parent-message :from)
+                            ))))
+         (maildir (when mu4e-compose-parent-message
+                    (mu4e-message-field mu4e-compose-parent-message :maildir)))
+         (account-candidates
+          (cond
+           (emails (delq nil (mapcar
+                              (lambda (x) (when (member (downcase (cadr (assoc 'user-mail-address x))) emails)
+                                            (car x)))
+                              mu4e-account-alist)))
+           (maildir (progn
+             (string-match "/\\(.*?\\)/" maildir)
+             (list (match-string 1 maildir))))
+           (t (mapcar 'car mu4e-account-alist))))
+         (account
+          (if (equal (length account-candidates) 1)
+              (car account-candidates)
+            (completing-read "Compose with account:" account-candidates)))
+         (account-vars (cdr (assoc account mu4e-account-alist))))
+    (if account-vars
+        (mu4e//map-set account-vars)
+      (error "No email account found"))))
+
+
 (defun ab2/mu4e-set-account ()
   "Set the account for composing a message. If composing new,
    let's the user chose, and otherwise us the to field"
