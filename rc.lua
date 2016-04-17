48f4eeca97992adc6dffffdf03572f2b9ffbb203
Abdo Roig-Maranges <abdo.roig@gmail.com>
2016-04-17 12:51:27 +0200

    tag names are strings

--- rc.lua
+++ rc.lua
@@ -122,7 +122,7 @@ layouts = {
 tags = {}
 
 awful.screen.connect_for_each_screen(function(s)
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, s, layouts[1])
+    tags[s] = awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" }, s, layouts[1])
 end)
 
 
