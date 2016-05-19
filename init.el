a31e0f53cac28d07b42a6c97ab8ce6770908b257
Abdo Roig-Maranges <abdo.roig@gmail.com>
2016-05-19 23:15:23 +0200

    add semantic layer

--- init.el
+++ init.el
@@ -65,6 +65,7 @@ values."
      (c-c++ :variables
             c-c++-default-mode-for-headers 'c++-mode
             c-c++-enable-clang-support t)
+     semantic
      asciidoc
      bibtex
      emacs-lisp
