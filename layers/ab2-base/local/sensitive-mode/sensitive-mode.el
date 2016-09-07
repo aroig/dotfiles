0a85dc0639cda87bfb470e7cc7dffef89746ced0
Abdo Roig-Maranges <abdo.roig@gmail.com>
2016-09-07 14:52:50 +0200

    properly enable sensitive-mode instead of replacing a major-mode

--- layers/ab2-base/local/sensitive-mode/sensitive-mode.el
+++ layers/ab2-base/local/sensitive-mode/sensitive-mode.el
@@ -7,23 +7,21 @@ With no argument, this command toggles the mode.
 Non-null prefix argument turns on the mode.
 Null prefix argument turns off the mode."
   ;; The initial value.
-  nil
+  :init-value nil
   ;; The indicator for the mode line.
-  " Sensitive"
+  :lighter " Sensitive"
   ;; The minor mode bindings.
-  nil
+  :keymap nil
   (if (symbol-value sensitive-mode)
       (progn
-	;; disable backups
-	(set (make-local-variable 'backup-inhibited) t)	
-	;; disable auto-save
-	(if auto-save-default
-	    (auto-save-mode -1)))
-    ;resort to default value of backup-inhibited
+        ;; disable backups
+        (set (make-local-variable 'backup-inhibited) t)
+        ;; disable auto-save
+        (when auto-save-default (auto-save-mode -1)))
+    ;; resort to default value of backup-inhibited
     (kill-local-variable 'backup-inhibited)
-    ;resort to default auto save setting
-    (if auto-save-default
-	(auto-save-mode 1))))
+    ;; resort to default auto save setting
+    (when auto-save-default (auto-save-mode 1))))
 
 
 (provide 'sensitive-mode)
