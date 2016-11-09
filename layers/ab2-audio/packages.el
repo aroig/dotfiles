dc88db09f5c83543d0c08e579ea7e4203985734c
Abdo Roig-Maranges <abdo.roig@gmail.com>
2016-11-09 13:47:01 +0100

    tweak audio layer

--- layers/ab2-audio/packages.el
+++ layers/ab2-audio/packages.el
@@ -1,7 +1,8 @@
 (setq ab2-audio-packages
-      '(extempore-mode
-        )
+      '(extempore-mode)
       )
 
 (defun ab2-audio/init-extempore-mode ()
-  (use-package extempore-mode :defer t))
+  (use-package extempore-mode
+    :mode ("\\.xtm\\'" . extempore-mode)
+    :defer t))
