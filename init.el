ea824b4413ad737a5c39f801e906901e153eb66b
Abdo Roig-Maranges <abdo.roig@gmail.com>
2017-01-26 21:56:20 +0100

    themes

--- init.el
+++ init.el
@@ -218,16 +218,7 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(zenburn
-                         spacemacs-dark
-                         base16-default-dark
-                         base16-railscasts-dark
-                         base16-eighties-dark
-                        ; spacemacs-light
-                        ; solarized-light
-                        ; solarized-dark
-                        ; leuven
-                        ; monokai
-                         )
+                         spacemacs-dark)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
