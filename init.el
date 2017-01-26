aa2c71114f401d2db9a95273beb5270d0238d27b
Abdo Roig-Maranges <abdo.roig@gmail.com>
2017-01-26 21:58:24 +0100

    update init

--- init.el
+++ init.el
@@ -219,7 +219,7 @@ values."
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(zenburn
                          spacemacs-dark)
-   ;; If non nil the cursor color matches the state color in GUI Emacs.
+   ;; If non-nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
