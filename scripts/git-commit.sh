24f0e6b46eae72b30ae693670fb517278d2c9977
Abdo Roig-Maranges <abdo.roig@gmail.com>
2017-02-01 12:07:38 +0100

    script cleanup

--- scripts/git-commit.sh
+++ scripts/git-commit.sh
@@ -12,19 +12,21 @@ if [ ! -d ".git" ]; then
 fi
 
 # check for changes
-num=$(git status --porcelain | wc -l)    
-if [[ $num -ge 1 ]]; then            
-    echo "adding files to '$repo'"    
-    [ -d ".git/annex" ] && git annex add . > /dev/null
+num=$(git status --porcelain | wc -l)
+if [[ $num -ge 1 ]]; then
+    echo "adding files to '$repo'"
+    if [ -d ".git/annex" ]; then
+        git annex add . > /dev/null
+    fi
     git add -A . > /dev/null
 else
     exit 0
 fi
-                
+
 # commit changes
 host=$(hostname -s)
 num=$(git status --porcelain | wc -l)
-if [[ $num -ge 1 ]]; then            
-    echo "commiting files to '$repo'"            
+if [[ $num -ge 1 ]]; then
+    echo "commiting files to '$repo'"
     git commit -q -m "auto-commit on $host ($num files)"
 fi
