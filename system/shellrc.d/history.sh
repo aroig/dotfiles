05baa59595809ffb77d1d7be4293e5cbdffa663c
Abdo Roig-Maranges <abdo.roig@gmail.com>
2017-02-15 09:58:00 +0100

    fix shell history

--- system/shellrc.d/history.sh
+++ system/shellrc.d/history.sh
@@ -2,7 +2,8 @@
 HISTSIZE=1000
 SAVEHIST=1000
 
-if [ "$BASH_VERSION" ]; then
+# bash history file
+if [ "$BASH_VERSION" ] && [ -n "$XDG_RUNTIME_DIR" ]; then
     HISTFILE="$XDG_RUNTIME_DIR/shell/bash_history"
     # extended globbing
     shopt -s extglob
@@ -16,9 +17,15 @@ if [ "$BASH_VERSION" ]; then
     sync_history() { history -a; history -c; history -r; }
 fi
 
-if [ "$ZSH_VERSION" ]; then
+# zsh history file
+if [ "$ZSH_VERSION" ] && [ -n "$XDG_RUNTIME_DIR" ]; then
     HISTFILE="$XDG_RUNTIME_DIR/shell/zsh_history"
     setopt incappendhistory
     setopt sharehistory
     setopt extendedhistory
+fi
+
+# Disable history if no XDG_RUNTIME_DIR is set
+if [ -z "$XDG_RUNTIME_DIR" ]; then
+    unset HISTFILE
 fi
