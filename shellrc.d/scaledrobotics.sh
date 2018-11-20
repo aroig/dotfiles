5538e3a2812f859b5ca6894465f494445b615110
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-11-20 01:38:09 +0100

    fix SR_WORKSPACE path

--- shellrc.d/scaledrobotics.sh
+++ shellrc.d/scaledrobotics.sh
@@ -1,5 +1,5 @@
 
 # Scaled Robotics code
-export SR_WORKSPACE="$HOME/work/scaledrobotics/home/code/workspace"
 export SR_ROSDISTRO="indigo"
+export SR_WORKSPACE="$HOME/work/scaledrobotics/home/$SR_ROSDISTRO/code/workspace"
 export SR_ROSROOT="/var/lib/machines/ros-$SR_ROSDISTRO"
