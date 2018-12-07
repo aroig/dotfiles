15ea0484895906ad03e2967dcd5cab147ea0b82b
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-12-07 01:26:18 +0100

    tweak prompt

--- system/shellrc.d/prompt.sh
+++ system/shellrc.d/prompt.sh
@@ -94,7 +94,7 @@ abdo_prompt_userhost() {
 
     case "$host" in
 	    grothendieck) hcol="${fg[yellow]}"       ;;
-        riemann)      hcol="${fg_bold[yellow]}"  ;;
+        riemann)      hcol="${fg[yellow]}"  ;;
         galois)       hcol="${fg_bold[red]}"     ;;
         skynet)       hcol="${fg_bold[cyan]}"    ;;
         quark)        hcol="${fg_bold[blue]}"    ;;
