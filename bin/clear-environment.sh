e29b05c1be276b73d21d6082460dee3f372d920d
Abdo Roig-Maranges <abdo.roig@gmail.com>
2018-12-23 23:26:50 +0100

    fix typo

--- bin/clear-environment.sh
+++ bin/clear-environment.sh
@@ -2,5 +2,5 @@
 
 export DISPLAY=:0
 
-systemctl --user unset-environment GDK_BACKEND QT_QPA_PLATFORM_CLUTTER_BACKEND SDL_VIDEODRIVER WAYLAND_DISPLA>Y
+systemctl --user unset-environment GDK_BACKEND QT_QPA_PLATFORM_CLUTTER_BACKEND SDL_VIDEODRIVER WAYLAND_DISPLAY
 systemctl --user import-environment DISPLAY
