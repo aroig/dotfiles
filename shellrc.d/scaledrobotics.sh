725bcae312a2e1ccf7e9bb89b71afa8c320938e6
Abdo Roig-Maranges <abdo.roig@gmail.com>
2019-03-15 22:39:32 +0100

    add alias to start ros container

--- shellrc.d/scaledrobotics.sh
+++ shellrc.d/scaledrobotics.sh
@@ -4,3 +4,9 @@ export SR_ROSDISTRO="melodic"
 export SR_WORKSPACE="$HOME/work/scaled/code/workspace"
 export SR_BUILDDIR="$HOME/build/scaled/home/$SR_ROSDISTRO/code/workspace/build"
 export SR_ROSROOT="/var/lib/machines/ros-$SR_ROSDISTRO"
+
+ros() {
+    local distro="${1-$SR_ROSDISTRO}"
+    sudo machinectl start "ros-$distro"
+    sudo machinectl shell "ros-$distro"
+}
